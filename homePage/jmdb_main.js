87cf7704e2ff8498337b8883bdda401a2e9ce36d
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2024-02-16 15:40:25 -0500

    1
    1

---
+++ homePage/jmdb_main.js
@@ -0,0 +1,7 @@
+
+
+function setup() {
+  //createCanvas(0, 0);
+
+
+}
---
+++ styles.js
@@ -0,0 +1,7 @@
+//const backgroundColor="#CBB677";
+const textColor="#FFFFFF";
+
+const Font= 'newFont';
+const errorColor= "#FF0000";
+const userColor= "#FFFFFF";
+const textFont="";
